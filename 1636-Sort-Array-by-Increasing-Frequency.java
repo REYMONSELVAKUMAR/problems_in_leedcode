246c0bd1ad471a7985c60a4cb3432b218947dccc
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2024-07-23 21:23:18 +0530

    [1636] [Time Beats: 92.24%] [Memory Beats: 14.86%] - LeetPush

--- 1636-Sort-Array-by-Increasing-Frequency.java
+++ 1636-Sort-Array-by-Increasing-Frequency.java
@@ -1,43 +1,24 @@
 class Solution {
-    public int[] frequencySort(int[] nums) {
-        int[] arr = new int[201];
-        int c=0;
-        for (int i : nums) {
-            arr[i + 100]++;
-            if(arr[i + 100]==1)c++;
+    public int[] frequencySort(int[] nums){
+    Map<Integer,TreeSet<Integer>>map=new TreeMap<>();
+        int[] a=new int[201];
+        for(int i:nums){
+            a[i+100]++;
         }
-        int[]a1=new int[c];
-        int[]a2=new int[c];
-        int l=0;
-        for(int i=0;i<201;i++){
-            if(arr[i]>0){
-                a1[l]=i-100;
-                a2[l]=arr[i];
-                l++;
+         for(int i:nums){
+            TreeSet<Integer> li=map.getOrDefault(a[100+i],new TreeSet<Integer>());
+            li.add(i);
+            map.put(a[100+i],li); 
+        }
+        int j=0;
+        for(int i:map.keySet()){
+            TreeSet<Integer> h=map.get(i);
+            while(!h.isEmpty()){
+                int k=0;
+                int e=h.pollLast();
+                 while(k++<i){
+                nums[j++]=e;
             }
-        }
-        for(int z=0;z<c;z++){
-            System.out.println(a1[z]+" "+a2[z]);
-        }
-        int tem1,tem2,m,k;
-        for(m=c-1;m>0;m--){
-            for(k=m-1;k>=0;k--){
-                if(a2[k]>a2[m]||(a2[k]==a2[m]&&a1[k]<a1[m])){
-                    tem1=a2[k];
-                    tem2=a1[k];
-                    a2[k]=a2[m];
-                    a2[m]=tem1;
-                    a1[k]=a1[m];
-                    a1[m]=tem2;
-                }
-            }
-        }
-        int g=0;
-        for(k=0;k<c;k++){
-            while(a2[k]>0){
-              nums[g]=a1[k];
-              a2[k]--;
-              g++;
             }
         }
         return nums;
