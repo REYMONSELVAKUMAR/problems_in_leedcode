cee5d42728d47387e4cd158c5770369c3504b0a9
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2024-09-04 14:14:14 +0530

    [3069] [Time Beats: 5.69%] [Memory Beats: 5.55%] - LeetPush

---
+++ 3069-Distribute-Elements-Into-Two-Arrays-I.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        ArrayList<Integer> a1=new ArrayList<>();
+        ArrayList<Integer> b1=new ArrayList<>();
+        a1.add(nums[0]);
+        b1.add(nums[1]);
+        int c=1;
+        for(int i=2;i<nums.length;i++){
+            int a,b;
+            a=a1.get(a1.size()-1);
+            b=b1.get(b1.size()-1);
+            if(a>b){
+                a1.add(nums[i]);
+                nums[c++]=nums[i];
+                System.out.println(a1);
+            }
+            else{
+                 b1.add(nums[i]);
+                 System.out.println(b1);
+            }
+        }
+        for(int d=0;c<nums.length;d++){
+            nums[c++]=b1.get(d);
+        }
+        return nums;
+        }
+}
