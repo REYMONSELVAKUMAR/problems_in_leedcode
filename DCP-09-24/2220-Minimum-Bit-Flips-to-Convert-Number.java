af10e13127e3c4d1d06fd21c7a27ed4a4e6bf5a8
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2024-09-11 14:33:27 +0530

    [2220] [Time Beats: 100.00%] [Memory Beats: 35.00%] - LeetPush

--- DCP-09-24/2220-Minimum-Bit-Flips-to-Convert-Number.java
+++ DCP-09-24/2220-Minimum-Bit-Flips-to-Convert-Number.java
@@ -1,5 +1,12 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        return Integer.bitCount(start^goal);
+       int a=start^goal,c=0;
+       while(a>0){
+        if(a%2==1){
+           c++;
+        }
+        a>>=1;
+       }
+       return c;
     }
 }
