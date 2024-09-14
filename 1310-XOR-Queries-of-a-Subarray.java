a2bffb13867b86ce96afd81d62b4d31b0cb12cad
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2024-09-14 09:19:23 +0530

    [1310] [Time Beats: 100.00%] [Memory Beats: 68.35%] - LeetPush

---
+++ 1310-XOR-Queries-of-a-Subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] a=new int[arr.length];
+        int[] b=new int[queries.length];
+         a[0]=arr[0];
+        for(int i=1;i<arr.length;i++){
+             a[i]=arr[i]^a[i-1];
+        }
+        for(int i=0;i<queries.length;i++){
+            if(queries[i][0]==0)b[i]=a[queries[i][1]];
+            else
+            b[i]=a[queries[i][1]]^a[queries[i][0]-1];
+        }
+        return b;
+    }
+}
