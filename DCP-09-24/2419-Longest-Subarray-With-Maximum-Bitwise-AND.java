3550d4c07367007e68ba4017271d958fbc6aab38
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2024-09-14 09:47:20 +0530

    [2419] [Time Beats: 89.89%] [Memory Beats: 14.61%] - LeetPush

--- DCP-09-24/2419-Longest-Subarray-With-Maximum-Bitwise-AND.java
+++ DCP-09-24/2419-Longest-Subarray-With-Maximum-Bitwise-AND.java
@@ -1,15 +1,18 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int max=0,c=0,m=0;
-        for(int i:nums){
-            if(max<i)max=i;
+      int max=0,maxl=0,c=0;
+      for(int i:nums){
+        max=Math.max(i,max);
+      }
+      for(int i:nums){
+        if(i==max){
+            c++;
+            maxl=Math.max(c,maxl);
         }
-        for(int i:nums){
-            if(i==max)c++;
-            else c=0;
-            if(c>m)m=c;
+        else{
+            c=0;
         }
-        return m;
-
+      }
+        return  maxl;
     }
 }
