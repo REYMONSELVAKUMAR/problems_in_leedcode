17568e255f0cd0acad0d64914fd53b6027d693f2
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2025-02-03 14:00:12 +0530

    [3084] [Time Beats: 44.87%] [Memory Beats: 94.30%] - LeetPush

--- 3084-Count-Substrings-Starting-and-Ending-with-Given-Character.java
+++ 3084-Count-Substrings-Starting-and-Ending-with-Given-Character.java
@@ -1,11 +1,12 @@
 class Solution {
     public long countSubstrings(String s, char c) {
-        long ct=0;
+        long ct=0,ans=0;
         for(char ch:s.toCharArray()){
             if(ch==c){
             ct++;
+            ans+=ct;
             }
         }
-        return (ct*(ct+1))/2;
+        return ans;
     }
 }
