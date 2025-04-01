fb50c01984ffc5491c6162bd3f6ac696cae6981f
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2025-04-01 15:25:26 +0530

    [15] [Time Beats: 67.78%] [Memory Beats: 71.55%] - LeetPush

--- 3Sum.java
+++ 3Sum.java
@@ -12,7 +12,6 @@ class Solution {
                 if(sum==0){
                     l.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     while (j < k && nums[j] == nums[j + 1]) j++;
-                    while (j < k && nums[k] == nums[k - 1]) k--;
                     j++;
                     k--;
                 }
