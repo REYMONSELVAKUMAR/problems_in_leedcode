5d55f03ac3ba3c955ab0cdd5e6783e530e1a1cd6
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2025-06-04 12:16:13 +0530

    [3403] [Time Beats: 57.41%] [Memory Beats: 26.85%] - LeetPush

--- Find-the-Lexicographically-Largest-String-From-the-Box-I.java
+++ Find-the-Lexicographically-Largest-String-From-the-Box-I.java
@@ -4,11 +4,7 @@ class Solution {
         if(numFriends==1)return word;
          int len=word.length()-numFriends+1;
         for(int i=0;i<word.length();i++){
-            if((i+len)>word.length()){
-                 tem=word.substring(i,word.length());
-            }
-            else
-              tem=word.substring(i,i+len);
+              tem=word.substring(i,Math.min(word.length(),i+len));
             if(tem.compareTo(ans)>0){
                 ans=tem;
             }
